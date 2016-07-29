190dc448a7243aebfb43a513017a755e2321581b
cttbot <chanhchung89@gmail.com>
2016-07-29 21:49:38 +0700

    Update
    BETA Port L#

---
+++ KurisuRiven/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using LeagueSharp.Common;
+using EloBuddy.SDK.Events;
+
+namespace KurisuRiven
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            new KurisuRiven();
+        }
+    }
+}
