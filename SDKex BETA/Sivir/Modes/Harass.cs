98ca0ff293cd352d7fb96342da34adc2bba2abac
cttbot <chanhchung89@gmail.com>
2016-08-05 00:35:09 +0700

    BETA SDKex

---
+++ SDKex BETA/Sivir/Modes/Harass.cs
@@ -0,0 +1,27 @@
+﻿using LeagueSharp.SDK;
+
+using Settings = Sivir.Config.Modes.Harass;
+
+namespace Sivir.Modes
+{
+    internal sealed class Harass : ModeBase
+    {
+        internal override bool ShouldBeExecuted()
+        {
+            return Config.Keys.HarassActive;
+        }
+
+        internal override void Execute()
+        {
+            if (!Variables.Orbwalker.CanMove)
+            {
+                return;
+            }
+
+            if(Settings.UseQ && Q.IsReady() && GameObjects.Player.ManaPercent > Settings.MinMana)
+            {
+                Q.CastOnBestTarget(0, true);
+            }
+        }
+    }
+}
---
+++ SDKex BETA/Sivir/Modes/JungleClear.cs
@@ -0,0 +1,23 @@
+﻿using EloBuddy;
+using LeagueSharp.SDK;
+
+using Settings = Sivir.Config.Modes.JungleClear;
+
+namespace Sivir.Modes
+{
+    internal sealed class JungleClear : ModeBase
+    {
+        internal override bool ShouldBeExecuted()
+        {
+            return Config.Keys.JungleClearActive;
+        }
+
+        internal override void Execute()
+        {
+            //if (!Variables.Orbwalker.CanMove)
+            //{
+            //    return;
+            //}
+        }
+    }
+}
