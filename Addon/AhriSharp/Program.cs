c72f8071e68982bd92c10ffda9bdd6047f9eb827
cttbot <chanhchung89@gmail.com>
2016-08-23 20:24:30 +0700

    Ahri

---
+++ Addon/AhriSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EloBuddy;
+using LeagueSharp.Common;
+
+namespace AhriSharp
+{
+    class Program
+    {
+        public static Helper Helper;
+
+        private static void Main(string[] args)
+        {
+            EloBuddy.SDK.Events.Loading.OnLoadingComplete += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Helper = new Helper();
+            new Ahri();
+        }
+    }
+}
