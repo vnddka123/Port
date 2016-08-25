64638855e8a7114e0fcef99eaf1c1efd634a08b8
cttbot <chanhchung89@gmail.com>
2016-08-25 10:06:09 +0700

    update

---
+++ SDKexLib/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="AssemblyInfo.cs" company="LeagueSharp">
+//    Copyright (c) 2015 LeagueSharp.
+// 
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+// 
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+// 
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/
+// </copyright>
+
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyTitle("LeagueSharp.SDKEx")]
+[assembly: AssemblyDescription("LeagueSharp Experimental Software Development Kit")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LeagueSharp")]
+[assembly: AssemblyProduct("LeagueSharp SDKEx")]
+[assembly: AssemblyCopyright("Copyright © LeagueSharp 2016")]
+[assembly: AssemblyTrademark("LeagueSharp")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e4860c36-20f0-4ca9-ad94-aa71eae94e8e")]
+[assembly: AssemblyVersion("3.0.1.7")]
+[assembly: AssemblyFileVersion("3.0.1.7")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AllowPartiallyTrustedCallers]
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+//[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[module: SuppressIldasm]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
